4f16a19e9ede00492c5313c9700ab0471c534d93
Teodora Mihoc <te.mihoc@gmail.com>
2025-01-29 09:15:27 +0100

    format code, fix redis on attribute

--- charm/src/charm.py
+++ charm/src/charm.py
@@ -8,18 +8,17 @@ import logging
 
 import ops
 
-from charms.data_platform_libs.v0.data_interfaces import (
-    DatabaseCreatedEvent,
-    DatabaseRequires,
-)
-
-from charms.redis_k8s.v0.redis import RedisRequires
+from charms.data_platform_libs.v0.data_interfaces import DatabaseRequires
+from charms.redis_k8s.v0.redis import RedisRequires, RedisRelationCharmEvents
 
 logger = logging.getLogger(__name__)
 
 
 class OverleafK8S2Charm(ops.CharmBase):
-    """Charm the application."""    
+    """Charm the application."""
+
+    on = RedisRelationCharmEvents()
+
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         framework.observe(self.on["community"].pebble_ready, self._configure_change)
@@ -28,18 +27,18 @@ class OverleafK8S2Charm(ops.CharmBase):
         self.database = DatabaseRequires(self, relation_name="database", database_name="overleaf")
         self.framework.observe(self.database.on.database_created, self._configure_change)
         self.redis = RedisRequires(self, "redis")
-        self.framework.observe(self.redis.on.redis_relation_updated, self._configure_change)
+        self.framework.observe(self.on.redis_relation_updated, self._configure_change)
 
     def _configure_change(self, event: ops.HookEvent):
         """Handle pebble-ready event."""
         # TODO If we don't have the database, we can't continue:
-        if not self.model.get_relation('database'):
+        if not self.model.get_relation("database"):
             logger.info("No relation to the MongoDB database yet.")
-            return # here's where the holistic approach makes thing easy -- you don't need to defer
-        if not self.model.get_relation('redis'):
+            return  # here's where the holistic approach makes thing easy -- you don't need to defer
+        if not self.model.get_relation("redis"):
             logger.info("No relation to the Redis database yet.")
-            return # here's where the holistic approach makes thing easy -- you don't need to defer
-        container = self.unit.containers["community"]           
+            return  # here's where the holistic approach makes thing easy -- you don't need to defer
+        container = self.unit.containers["community"]
         if not container.can_connect():
             logger.info("Pebble not ready yet.")
             return
@@ -129,7 +128,7 @@ http {
 	include /etc/nginx/sites-enabled/*;
 }
 
-        """ 
+        """
         container.push("/etc/nginx/nginx.conf", config)
 
         # Make Pebble reevaluate its plan, ensuring any services are started if enabled.
@@ -182,98 +181,101 @@ http {
 
         return data
 
-
     @property
     def _pebble_layer(self):
-        """Return a dictionary representing a Pebble layer.""" 
-        # The services are in overleaf/server-ce/runit. 
-        # There's 14 of them. 
+        """Return a dictionary representing a Pebble layer."""
+        # The services are in overleaf/server-ce/runit.
+        # There's 14 of them.
         # When you look at each, there's a run script.
         # In that script, the relevant lines are the last 3:
-        # source /etc/overleaf/env.sh 
+        # source /etc/overleaf/env.sh
         # This one holds the common variables.
         # export LISTEN_ADDRESS=127.0.0.1
         # This one holds the variables specific for the service.
         # exec /sbin/setuser www-data /usr/bin/node $NODE_PARAMS /overleaf/services/chat/app.js >> /var/log/overleaf/chat.log 2>&1
-        # This one has the user, 'www-data', and the actual command. We copy it without $NODE_PARAMS as that's not necessary for now.    
+        # This one has the user, 'www-data', and the actual command. We copy it without $NODE_PARAMS as that's not necessary for now.
         database_settings = self.get_relation_data()
         common_env = {
-	"CHAT_HOST":"127.0.0.1",
-	"CLSI_HOST":"127.0.0.1",
-	"CONTACTS_HOST":"127.0.0.1",
-	"DOCSTORE_HOST":"127.0.0.1",
-	"DOCUMENT_UPDATER_HOST":"127.0.0.1",
-	"DOCUPDATER_HOST":"127.0.0.1",
-	"FILESTORE_HOST":"127.0.0.1",
-	"HISTORY_V1_HOST":"127.0.0.1",
-    "MONGO_ENABLED": "false",
-    "MONGO_URL":f"mongodb://{database_settings['MONGO_USER']}:{database_settings['MONGO_PASSWORD']}@{database_settings['MONGO_HOST']}:{database_settings['MONGO_PORT']}/{database_settings['MONGO_DB']}",
-	"NOTIFICATIONS_HOST":"127.0.0.1",
-	"PROJECT_HISTORY_HOST":"127.0.0.1",
-	"REALTIME_HOST":"127.0.0.1",
-    "REDIS_ENABLED": "false",
-    "REDIS_HOST": self.redis.relation_data.get("hostname"),
-	"SPELLING_HOST":"127.0.0.1",
-	"WEB_HOST":"127.0.0.1",
-	"WEB_API_HOST":"127.0.0.1",               
-               }   
-#         command = "/sbin/my_init" 
-# #TODO figure out the right command (pebble plan seems to be working but the workload service isn't running)
-#         env = {
-#              "OVERLEAF_IMAGE_NAME":"sharelatex/sharelatex",
-# "OVERLEAF_DATA_PATH":"data/overleaf",
-# "SERVER_PRO":"false",
-# "OVERLEAF_LISTEN_IP":"127.0.0.1",
-# "OVERLEAF_PORT":"80",
-# "SIBLING_CONTAINERS_ENABLED":"true",
-# "DOCKER_SOCKET_PATH":"/var/run/docker.sock",
-# "MONGO_ENABLED":"true", #actually means overleaf should have its own mongodb
-# "MONGO_DATA_PATH":"data/mongo",
-# "MONGO_IMAGE":"mongo",
-# "MONGO_VERSION":"6.0",
-# "REDIS_ENABLED":"true",
-# "REDIS_DATA_PATH":"data/redis",
-# "REDIS_IMAGE":"redis:6.2",
-# "REDIS_AOF_PERSISTENCE":"true",
-# "GIT_BRIDGE_ENABLED":"false",
-# "GIT_BRIDGE_DATA_PATH":"data/git-bridge",
-# "NGINX_ENABLED":"false",
-# "NGINX_CONFIG_PATH":"config/nginx/nginx.conf",
-# "NGINX_HTTP_PORT":"80",
-# "NGINX_HTTP_LISTEN_IP":"127.0.1.1",
-# "NGINX_TLS_LISTEN_IP":"127.0.1.1",
-# "TLS_PRIVATE_KEY_PATH":"config/nginx/certs/overleaf_key.pem",
-# "TLS_CERTIFICATE_PATH":"config/nginx/certs/overleaf_certificate.pem",
-# "TLS_PORT":"443",
-# "OVERLEAF_APP_NAME":"Our Overleaf Instance",
-# "ENABLED_LINKED_FILE_TYPES":"project_file,project_output_file",
-# "ENABLE_CONVERSIONS":"true",
-# "EMAIL_CONFIRMATION_DISABLED":"true",
-#         }
-        chat_env = common_env.copy() 
-        chat_env.update({"LISTEN_ADDRESS":"127.0.0.1"})
-        clsi_env = common_env.copy() 
-        clsi_env.update({"LISTEN_ADDRESS":"127.0.0.1"})        
-        spelling_env = common_env.copy() 
-        spelling_env.update({"LISTEN_ADDRESS":"127.0.0.1"})
-        contacts_env = common_env.copy() 
-        contacts_env.update({"LISTEN_ADDRESS":"127.0.0.1"})    
-        docstore_env = common_env.copy() 
-        docstore_env.update({"LISTEN_ADDRESS":"127.0.0.1"})  
-        document_updater_env = common_env.copy() 
-        document_updater_env.update({"LISTEN_ADDRESS":"127.0.0.1"})               
-        filestore_env = common_env.copy() 
-        filestore_env.update({"LISTEN_ADDRESS":"127.0.0.1"})                                             
-        notifications_env = common_env.copy() 
-        notifications_env.update({"LISTEN_ADDRESS":"127.0.0.1"})                       
-        project_history_env = common_env.copy() 
-        project_history_env.update({"LISTEN_ADDRESS":"127.0.0.1"})                       
-        real_time_env = common_env.copy() 
-        real_time_env.update({"LISTEN_ADDRESS":"127.0.0.1"})                       
-        web_api_env = common_env.copy() 
-        web_api_env.update({"LISTEN_ADDRESS":"0.0.0.0", "ENABLED_SERVICES":"api", "METRICS_APP_NAME":"web-api"})                                                                                                                                                      
-        web_env = common_env.copy() 
-        web_env.update({"LISTEN_ADDRESS":"127.0.0.1", "ENABLED_SERVICES":"web", "WEB_PORT":"4000"})                       
+            "CHAT_HOST": "127.0.0.1",
+            "CLSI_HOST": "127.0.0.1",
+            "CONTACTS_HOST": "127.0.0.1",
+            "DOCSTORE_HOST": "127.0.0.1",
+            "DOCUMENT_UPDATER_HOST": "127.0.0.1",
+            "DOCUPDATER_HOST": "127.0.0.1",
+            "FILESTORE_HOST": "127.0.0.1",
+            "HISTORY_V1_HOST": "127.0.0.1",
+            "MONGO_ENABLED": "false",
+            "MONGO_URL": f"mongodb://{database_settings['MONGO_USER']}:{database_settings['MONGO_PASSWORD']}@{database_settings['MONGO_HOST']}:{database_settings['MONGO_PORT']}/{database_settings['MONGO_DB']}",
+            "NOTIFICATIONS_HOST": "127.0.0.1",
+            "PROJECT_HISTORY_HOST": "127.0.0.1",
+            "REALTIME_HOST": "127.0.0.1",
+            "REDIS_ENABLED": "false",
+            "REDIS_HOST": self.redis.relation_data.get("hostname"),
+            "SPELLING_HOST": "127.0.0.1",
+            "WEB_HOST": "127.0.0.1",
+            "WEB_API_HOST": "127.0.0.1",
+        }
+        #         command = "/sbin/my_init"
+        # #TODO figure out the right command (pebble plan seems to be working but the workload service isn't running)
+        #         env = {
+        #              "OVERLEAF_IMAGE_NAME":"sharelatex/sharelatex",
+        # "OVERLEAF_DATA_PATH":"data/overleaf",
+        # "SERVER_PRO":"false",
+        # "OVERLEAF_LISTEN_IP":"127.0.0.1",
+        # "OVERLEAF_PORT":"80",
+        # "SIBLING_CONTAINERS_ENABLED":"true",
+        # "DOCKER_SOCKET_PATH":"/var/run/docker.sock",
+        # "MONGO_ENABLED":"true", #actually means overleaf should have its own mongodb
+        # "MONGO_DATA_PATH":"data/mongo",
+        # "MONGO_IMAGE":"mongo",
+        # "MONGO_VERSION":"6.0",
+        # "REDIS_ENABLED":"true",
+        # "REDIS_DATA_PATH":"data/redis",
+        # "REDIS_IMAGE":"redis:6.2",
+        # "REDIS_AOF_PERSISTENCE":"true",
+        # "GIT_BRIDGE_ENABLED":"false",
+        # "GIT_BRIDGE_DATA_PATH":"data/git-bridge",
+        # "NGINX_ENABLED":"false",
+        # "NGINX_CONFIG_PATH":"config/nginx/nginx.conf",
+        # "NGINX_HTTP_PORT":"80",
+        # "NGINX_HTTP_LISTEN_IP":"127.0.1.1",
+        # "NGINX_TLS_LISTEN_IP":"127.0.1.1",
+        # "TLS_PRIVATE_KEY_PATH":"config/nginx/certs/overleaf_key.pem",
+        # "TLS_CERTIFICATE_PATH":"config/nginx/certs/overleaf_certificate.pem",
+        # "TLS_PORT":"443",
+        # "OVERLEAF_APP_NAME":"Our Overleaf Instance",
+        # "ENABLED_LINKED_FILE_TYPES":"project_file,project_output_file",
+        # "ENABLE_CONVERSIONS":"true",
+        # "EMAIL_CONFIRMATION_DISABLED":"true",
+        #         }
+        chat_env = common_env.copy()
+        chat_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        clsi_env = common_env.copy()
+        clsi_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        spelling_env = common_env.copy()
+        spelling_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        contacts_env = common_env.copy()
+        contacts_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        docstore_env = common_env.copy()
+        docstore_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        document_updater_env = common_env.copy()
+        document_updater_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        filestore_env = common_env.copy()
+        filestore_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        notifications_env = common_env.copy()
+        notifications_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        project_history_env = common_env.copy()
+        project_history_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        real_time_env = common_env.copy()
+        real_time_env.update({"LISTEN_ADDRESS": "127.0.0.1"})
+        web_api_env = common_env.copy()
+        web_api_env.update(
+            {"LISTEN_ADDRESS": "0.0.0.0", "ENABLED_SERVICES": "api", "METRICS_APP_NAME": "web-api"}
+        )
+        web_env = common_env.copy()
+        web_env.update(
+            {"LISTEN_ADDRESS": "127.0.0.1", "ENABLED_SERVICES": "web", "WEB_PORT": "4000"}
+        )
         pebble_layer = {
             # TODO: 3 services not running: history_v1, nginx, real-time
             "summary": "Overleaf service",
@@ -294,7 +296,7 @@ http {
                     "startup": "enabled",
                     "environment": clsi_env,
                     "user": "www-data",
-                },                
+                },
                 "contacts": {
                     "override": "replace",
                     "summary": "contacts",
@@ -302,7 +304,7 @@ http {
                     "startup": "enabled",
                     "environment": contacts_env,
                     "user": "www-data",
-                },     
+                },
                 "docstore": {
                     "override": "replace",
                     "summary": "docstore",
@@ -310,7 +312,7 @@ http {
                     "startup": "enabled",
                     "environment": docstore_env,
                     "user": "www-data",
-                },  
+                },
                 "document_updater": {
                     "override": "replace",
                     "summary": "document updater",
@@ -318,7 +320,7 @@ http {
                     "startup": "enabled",
                     "environment": document_updater_env,
                     "user": "www-data",
-                },   
+                },
                 "filestore": {
                     "override": "replace",
                     "summary": "filestore",
@@ -326,15 +328,15 @@ http {
                     "startup": "enabled",
                     "environment": filestore_env,
                     "user": "www-data",
-                },  
+                },
                 "history_v1": {
                     "override": "replace",
                     "summary": "history v1",
                     "command": "/usr/bin/node /overleaf/services/history-v1/app.js >> /var/log/overleaf/history-v1.log 2>&1",
                     "startup": "enabled",
-                    "environment": {"NODE_CONFIG_DIR":"/overleaf/services/history-v1/config"}, 
+                    "environment": {"NODE_CONFIG_DIR": "/overleaf/services/history-v1/config"},
                     "user": "www-data",
-                },                   
+                },
                 "nginx": {
                     "override": "replace",
                     "summary": "nginx",
@@ -342,8 +344,8 @@ http {
                     "startup": "enabled",
                     "environment": {},
                     "user": "www-data",
-                }, 
-                  "notifications": {
+                },
+                "notifications": {
                     "override": "replace",
                     "summary": "notifications",
                     "command": "/usr/bin/node /overleaf/services/notifications/app.js >> /var/log/overleaf/notifications.log 2>&1",
@@ -366,7 +368,7 @@ http {
                     "startup": "enabled",
                     "environment": real_time_env,
                     "user": "www-data",
-                },                   
+                },
                 "spelling": {
                     "override": "replace",
                     "summary": "spelling",
@@ -382,7 +384,7 @@ http {
                     "startup": "enabled",
                     "environment": web_api_env,
                     "user": "www-data",
-                },                   
+                },
                 "web": {
                     "override": "replace",
                     "summary": "web",
@@ -390,11 +392,11 @@ http {
                     "startup": "enabled",
                     "environment": web_env,
                     "user": "www-data",
-                },                                                                
-                
+                },
             },
         }
         return ops.pebble.Layer(pebble_layer)
+
 
 if __name__ == "__main__":  # pragma: nocover
     ops.main(OverleafK8S2Charm)  # type: ignore
